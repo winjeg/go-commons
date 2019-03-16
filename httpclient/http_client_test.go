c28c9a1acbf832bc246574c74c37a832a9bea469
winjeg <winjeg@qq.com>
2019-03-17 00:04:32 +0800

    change test addr

--- httpclient/http_client_test.go
+++ httpclient/http_client_test.go
@@ -7,28 +7,28 @@ import (
 )
 
 func TestGet(t *testing.T) {
-	d, err := Get("https://www.so.com")
+	d, err := Get("https://www.bing.com")
 	assert.Equal(t, true, err == nil)
 	assert.Equal(t, true, len(d) > 0)
 }
 
 func TestGetWithParams(t *testing.T) {
 	m := make(map[string]string, 2)
-	m["ie"] = "utf-8"
+	m["qs"] = "utf-8"
 	m["q"] = "test"
-	d, err := GetWithParams("https://www.so.com/s", m)
+	d, err := GetWithParams("https://www.bing.com", m)
 	assert.Equal(t, true, err == nil)
 	assert.Equal(t, true, len(d) > 0)
-	w, err2 := GetWithParams("https://www.so.com/sasdasdas", nil)
+	w, err2 := GetWithParams("https://www.bing.com/sasdasdas", nil)
 	assert.Nil(t, err2)
 	assert.Equal(t, true, len(w) == 0)
 }
 
 func TestPostDelete(t *testing.T) {
-	r1, err := Delete("https://www.so.com/api/dx", "")
+	r1, err := Delete("https://cn.bing.com/api/dx", "")
 	assert.NotNil(t, err)
 	assert.Empty(t, r1)
-	r2, err := Put("https://www.so.com/api/dx", "")
+	r2, err := Put("https://cn.bing.com/api/dx", "")
 	assert.NotNil(t, err)
 	assert.Empty(t, r2)
 }
